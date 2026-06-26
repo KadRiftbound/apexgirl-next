9df96b65e4fca0be1973de34e3bd099fa20bac88
KadRiftbound <christophe.magnankad@gmail.com>
2026-06-26 11:02:13 +0200

    Add Grow Journey (grow.me) engagement script to layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -139,6 +139,7 @@ export default async function LocaleLayout({
   return (
     <>
       <Analytics />
+      <script data-grow-initializer="" dangerouslySetInnerHTML={{ __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTpiMTI1NDRjMS1lMjk3LTRiZjAtOWZlNy1mNzRkYTA0NWFiOGY=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();` }} />
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
